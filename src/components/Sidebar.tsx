32145ec6312780d4b0ba96bec628dcd16566119d
Ade Naufal Ammar <ade.naufal@gmail.com>
2025-07-22 16:12:54 +0700

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,8 +25,8 @@ export function Sidebar({
   onColorSelect,
 }: SidebarProps) {
   return (
-    <aside className="w-80 bg-white border-r border-gray-200 flex-shrink-0">
-      <div className="p-4 space-y-2">
+    <aside className="w-80 bg-white border-r border-gray-200 flex-shrink-0 h-full flex flex-col">
+      <div className="p-4 space-y-2 flex-1 overflow-y-auto">
         <ColorSection
           colors={gradientState.colors}
           onColorsChange={onColorsChange}
